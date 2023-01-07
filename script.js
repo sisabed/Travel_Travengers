fb34fef968bae17168b8558e27e6fb2c2334f19d
Debasis Sahoo <104120947+sisabed@users.noreply.github.com>
2023-01-08 01:04:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let menu= document.querySelector('#menu-icon');
+let navbar= document.querySelector('.navbar');
+let service=document.querySelector('.service-dropdown');
+let gallery= document.querySelector('.gallery')
+menu.onclick=() =>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+gallery.onclick=() =>{
+    
+    
+}
